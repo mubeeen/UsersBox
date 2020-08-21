63d2f165ab95e691f9383674cfa716eaf0700804
mubeen <mu.mubeen100989@gmail.com>
2020-08-21 19:42:38 +0500

    comm 3

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
 
-
 const btn1Style = {
     width:"20%",
     margin:"2px"
@@ -13,9 +12,12 @@ function home()
             <div class="container">
                 <h1 class="display-4">Welcome to User Box</h1>
                 <p class="lead">The site contain the dummy data of users</p>
-                <button style={btn1Style} id="top" type="button" class="btn btn-primary btn-lg">User</button>
-                <button style={btn1Style} type="button" class="btn btn-danger btn-lg" >Box</button>
-
+                <Link to="/users">
+                    <button style={btn1Style} id="top" type="button" class="btn btn-primary btn-lg">Users</button>
+                </Link>
+                <Link to="/users">
+                    <button style={btn1Style} type="button" class="btn btn-danger btn-lg" >Box</button>
+                </Link>
             </div>
         </div>
     )
